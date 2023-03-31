84d7d6733fe0725b3b152987fcd168908db3e142
Jekarter <jkartyalta@yandex.ru>
2023-03-31 19:00:50 +0500

    feat(card-statistic): add mock user and card-stat

---
+++ src/mock/index.ts
@@ -0,0 +1,17 @@
+import { LocalGithubUser } from "types";
+
+export const defaultUser: LocalGithubUser = {
+  "login": "Jekarter",
+  "id": 101813294,
+  "avatar": "https://avatars.githubusercontent.com/u/101813294?v=4",
+  "name": "Evgeniy",
+  "company": "Alfa-bank",
+  "blog": "mishaep.com",
+  "location": "Russia",
+  "bio": "Frontend-developer",
+  "twitter": "Jekarter",
+  "repos": 9,
+  "followers": 0,
+  "following": 0,
+  "created": "2022-03-17T13:40:51Z",
+}
